3ad384e00d031e0d10d3528f11564181414a84d5
BluffDragon8211 <lortega@edenschool.fr>
2024-06-18 17:22:48 +0200

    creating Item class

---
+++ js/model/item/Item.js
@@ -0,0 +1,16 @@
+import Canvas from '../Canvas.js'
+
+export default class Item {
+    constructor(file, tile, x, y ) {
+        this.canvas = new Canvas()
+        this.canvas.element.style.zIndex = 1
+        this.canvas.setStep(game.map.size)
+        this.file = file
+        this.tile = tile
+        this.position = {x:x, y:y}
+    }
+
+    display() {
+        this.canvas.draw(this.file, this.tile, {x: this.position.x, y: this.position.y})
+    }
+}
