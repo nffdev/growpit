2e4f448303fffc16e6714dd8e54935d63da73601
BluffDragon8211 <lortega@edenschool.fr>
2024-06-20 15:39:17 +0200

    Fix heuristic and documentation

--- js/model/astar/Node.js
+++ js/model/astar/Node.js
@@ -1,33 +1,23 @@
+/**
+ * Class Node: represents a square in the grid
+ */
 export default class Node {
     constructor(square) {
         this.position = square.position
         this.square = square
-        this.isBlocked = false
-        switch(square.tile.key) {
-            /* case "TREE":
-                this.isBlocked = true
-                break
-            case "GRASS":
-                this.weight = 0.7
-                break
-            case "GROUND":
-                this.weight = 0.6
-                break
-            case "BUISSON":
-                this.weight = 1.5
-                break */
-            default:
-                this.weight = square.tile.weight ? square.tile.weight : 0.6
-                this.isBlocked = square.tile.isBlocked ? square.tile.isBlocked : false
-                break
-        }
-        //this.isBlocked = square.tile.key == "TREE"
+        this.weight = square.tile.weight ? square.tile.weight : 0.6
+        this.isBlocked = square.tile.isBlocked ? square.tile.isBlocked : false
     }
 
+    /**
+     * Method heuristic: affect the weight for the node
+     * @param {Node} parent Parent node
+     * @param {Node} destination Destination node
+     */
     heuristic(parent, destination) {
         this.parent = parent
         this.g = this.parent.g + 1
-        this.h = (Math.abs(this.position.x - destination.x) + Math.abs(this.position.y - destination.y)) * this.weight
+        this.h = (Math.abs(this.position.x - destination.position.x) + Math.abs(this.position.y - destination.position.y)) * this.weight
         this.f = this.h + this.g
     }
 }
